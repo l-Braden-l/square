3a124b7d300c9bc2c499af144eb8793ace7c6153
braden <leachbr787@northwested.org>
2025-01-10 13:18:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+#Braden Leach
+#January 10 2024
+#Return Value Square
 
+
+#----1----#
+def square_number(num):
+    return num**2
+
+#----2----#
+def add_squares(num1,num2):
+    nums1 = square_number(num1)
+    nums2 = square_number(num2)
+    added =  nums1 + nums2
+    return  added
+
+#----3----#
+def main():
+    num1 =float(input('Enter the first number: '))
+    num2 =float(input('Enter the second number: '))
+    answer = add_squares(num1,num2)
+    print(f'The sum of the squares of {num1} and {num2} is: {answer:.2f}')
+
+
+main()
